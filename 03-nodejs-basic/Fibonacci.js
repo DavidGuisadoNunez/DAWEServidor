d259449556a1db1fd5fc09bb777f86c841c2da3d
DavidGuisadoNunez <152395644+DavidGuisadoNunez@users.noreply.github.com>
2024-09-28 13:07:34 +0200

    Corrección Fibonacci

--- 03-nodejs-basic/Fibonacci.js
+++ 03-nodejs-basic/Fibonacci.js
@@ -1,8 +1,8 @@
-let n = 15, a = 0, b = 1, temp;
+let n = 15, a = 0, b = 1, suma;
 console.log(a);
 for(let i = 1; i < n; i++){
     console.log(b);
-    temp = a + b;
+    suma = a + b;
     a = b;
-    b = temp;
+    b = suma;
 }

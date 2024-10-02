b0d6d5b9902f25771ca0a976c2657641bfc728d6
DavidGuisadoNunez <152395644+DavidGuisadoNunez@users.noreply.github.com>
2024-10-02 11:49:55 +0200

    Repaso node quirck

--- 04-nodejs-quirck/functions.js
+++ 04-nodejs-quirck/functions.js
@@ -1,7 +1,26 @@
-function name(params){
-    console.log(params);
+// function myFunc(params = 'Gabriel', params2= 'Rodríguez'){ // Esto toma que si no recibe parámetros, toma esto por defecto
+//     console.log(params);
+//     console.log(params, params2);
+// }
+
+// myFunc();
+// myFunc('Gabri', 123);
+
+
+
+function myFunc2(name, n, ...numbers) {
+    console.log(name);
+    console.log(numbers);
 }
 
-name();
-name('Gabri');
-name({name: 'Gabri'});
+myFunc2('Gabriel', 1,2,3,4,'Daviddd',5,6); // El 1 se guarda en n, y por eso no lo pinta.
+
+
+
+// Exactamente igual que la de arriba pero se guarda en una variable.
+const myVarFunc = function myFunc2(name, n, ...numbers) {
+    console.log(name);
+    console.log(numbers);
+}
+
+myVarFunc('Gabriel', 1,2,3,4,'Daviddd',5,6);

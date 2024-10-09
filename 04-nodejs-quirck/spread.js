04689c212713dba4eb5f3bab2f81b54f762d4f5b
DavidGuisadoNunez <152395644+DavidGuisadoNunez@users.noreply.github.com>
2024-10-09 16:28:57 +0200

    Eliminación archivos y carpetas

--- 04-nodejs-quirck/spread.js
+++
@@ -1,6 +0,0 @@
-const array1 = [1,2,3,4,5];
-const array2 = [10,20,30,40,50];
-
-const array3 = [...array1, ...array2]; // Se rompen dos arrays y el contenido se ambos se metería en un solo array (array3).
-
-console.log(...array3) // Se rompe el array3 y aparecen los números sin corchetes

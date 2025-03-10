607c698fcebb42b0204b160b98bdea63089d38ea
DavidGuisadoNunez <152395644+DavidGuisadoNunez@users.noreply.github.com>
2025-03-10 16:18:11 +0100

    Fixing authRoutes

--- backend-concesionario/src/routes/authRoutes.js
+++ backend-concesionario/src/routes/authRoutes.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import * as authController from '../controllers/authController.js';
+import { authMiddleware, adminMiddleware } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 /**
  * Rutas de autenticación
  */
-router.post('/register', authController.register);
+router.post('/register', authMiddleware, adminMiddleware, authController.register);
 router.post('/login', authController.login);
-router.post('/refresh-token', authController.refreshToken);
+router.post('/refresh-token', authMiddleware, authController.refreshToken);
 
 export default router;
